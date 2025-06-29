e15f6a3472b0a0f8743fe02e5e591c36da7c211a
CODonnie <ordudesigns@gmail.com>
2025-06-28 18:36:26 -0700

    body section structuring: div - adding section border design

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div className='flex justify-center items-center w-full'>
-            <div className='mobabout'></div>
+        <div className='flex justify-center items-center w-full relative'>
+            {/* fake border */}
+            <div className='mobabout absolute inset-0 z-0 border-mask bg-[hsl(var(--foreground))] scale-[1] translate-x-[0.3%] translate-y-[-0.9%]'></div>
+            {/* main div */}
+            <div className="mobabout relative z-10 bg-[hsl(var(--background))]"></div>
         </div>
     )
 }
--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='flex justify-center items-center w-full'>
-            <div className='mobhero'></div>
+        <div className='flex justify-center items-center w-full relative'>
+            {/* fake border */}
+            <div className='mobhero absolute inset-0 z-0 border-mask bg-[hsl(var(--foreground))] scale-[1] translate-x-[0.3%] translate-y-[0.8%]'></div>
+            {/* main div */}
+            <div className="mobhero relative z-10 bg-[hsl(var(--background))]"></div>
         </div>
     )
 }
